5d91c91e1f8648c7296d660be07be62c5cb07c2b
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-04-13 11:41:48 +0530

    added dynamic column rendering

---
+++ src/columns.tsx
@@ -0,0 +1,145 @@
+import { ToDoItem } from "./components/ToDoList";
+import moment from "moment";
+import { Tag } from "antd";
+import { ProColumns } from "@ant-design/pro-table";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import DeleteTask from "./components/DeleteTask";
+
+export const columns: ProColumns<ToDoItem>[] = [
+  {
+    title: "Timestamp created",
+    dataIndex: "created",
+    valueType: "dateTime",
+    width: 150,
+    editable: false,
+    defaultSortOrder: "descend",
+    render: (_, { created }) => <span>{moment(created).format("lll")}</span>,
+    sorter: (a, b) => a.created - b.created,
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+    width: 150,
+    ellipsis: true,
+    formItemProps: {
+      rules: [{ required: true }],
+    },
+    render: (_, { title }) => <span>{title}</span>,
+    sorter: (a, b) => a.title.localeCompare(b.title),
+  },
+  {
+    title: "Description",
+    dataIndex: "description",
+    width: 200,
+    ellipsis: true,
+    formItemProps: {
+      rules: [{ required: true }],
+    },
+    render: (_, { description }) => <span>{description}</span>,
+    sorter: (a, b) => a.description.localeCompare(b.description),
+  },
+  {
+    title: "Due Date",
+    dataIndex: "dueDate",
+    valueType: "dateTime",
+    width: 150,
+    formItemProps: {
+      rules: [{ type: "object" }],
+    },
+    render: (_, { dueDate }) => (
+      <span>
+        {dueDate ? moment(dueDate).format("MMM DD, YYYY") : "No due date"}
+      </span>
+    ),
+    sorter: (a, b) => (a.dueDate ?? 0) - (b.dueDate ?? 0),
+  },
+  {
+    title: "Tags",
+    dataIndex: "tags",
+    width: 150,
+    formItemProps: {
+      rules: [{ type: "array" }],
+    },
+    render: (_, { tags }) => (
+      <span>
+        {tags && tags.length > 0
+          ? tags.map((tag) => (
+              <Tag key={tag} color="purple">
+                {tag.toUpperCase()}
+              </Tag>
+            ))
+          : "No tags added"}
+      </span>
+    ),
+    filters: [
+      { text: "No Tags", value: "noTags" },
+      { text: "Tags Present", value: "tagsPresent" },
+    ],
+    onFilter: (value, record) => {
+      if (value === "noTags") {
+        return !record.tags || record.tags.length === 0;
+      } else if (value === "tagsPresent") {
+        return record.tags && record.tags.length > 0;
+      }
+      return false;
+    },
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    width: 150,
+    valueEnum: {
+      open: { text: "OPEN", status: "blue" },
+      working: { text: "WORKING", status: "yellow" },
+      done: { text: "DONE", status: "green" },
+      overdue: { text: "OVERDUE", status: "red" },
+    },
+    formItemProps: {
+      rules: [{ required: true }],
+    },
+    filters: [
+      { text: "OPEN", value: "OPEN" },
+      { text: "WORKING", value: "WORKING" },
+      { text: "DONE", value: "DONE" },
+      { text: "OVERDUE", value: "OVERDUE" },
+    ],
+    onFilter: (value, { status }) => status === value,
+    render: (text, { status }) => {
+      const tagType = {
+        OPEN: "blue",
+        WORKING: "yellow",
+        DONE: "green",
+        OVERDUE: "red",
+      }[status];
+      return <Tag color={tagType}>{status}</Tag>;
+    },
+  },
+  {
+    title: "Actions",
+    valueType: "option",
+    width: 150,
+    render: () => [
+      <>
+        <a
+          style={{
+            marginRight: 20,
+          }}
+          key="edit"
+          onClick={() => {
+            console.log("Edit");
+          }}
+        >
+          Edit{" "}
+          <EditOutlined
+            style={{
+              color: "blue",
+              marginLeft: "5px",
+            }}
+          />
+        </a>
+        <DeleteTask />
+      </>,
+      ,
+    ],
+  },
+];
