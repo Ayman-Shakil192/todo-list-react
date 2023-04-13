82ba636caa46d9912adec96a092c10cd6e0d8e1a
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-04-14 04:11:30 +0530

    added header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import "./App.css";
 import ToDoList from "./components/ToDoList";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
+      <nav>
+        <h3>AlgoBulls Frontend Developer Coding Assignment</h3>
+      </nav>
       <ToDoList />
+      <footer>
+        <p>
+          Developed by{" "}
+          <a
+            href="https://github.com/Ayman-Shakil192"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ayman Shakil
+          </a>
+        </p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
