b9bbeaa8c94c69159126ab0e0f0ce88dd9944582
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-04-14 04:11:52 +0530

    added random timestamps and due dates

--- src/dummyData.tsx
+++ src/dummyData.tsx
@@ -6,12 +6,26 @@ export const dummyData: ToDoItem[] = new Array(80).fill(0).map((_, index) => {
   for (let j = 1; j <= randomTagCount; j++) {
     tags.push(`tag${j}`);
   }
+  const randomDays = Math.floor(Math.random() * 365);
+  const randomHours = Math.floor(Math.random() * 24);
+  const randomMinutes = Math.floor(Math.random() * 60);
+  const randomSeconds = Math.floor(Math.random() * 60);
+  const createdAt = new Date(
+    Date.now() -
+      randomDays * 24 * 60 * 60 * 1000 -
+      randomHours * 60 * 60 * 1000 -
+      randomMinutes * 60 * 1000 -
+      randomSeconds * 1000
+  ).getTime();
+  const dueDate = new Date(
+    createdAt + Math.floor(Math.random() * 5 + 1) * 24 * 60 * 60 * 1000
+  ).getTime();
   return {
     key: index,
-    created: Date.now(),
+    created: createdAt,
     title: `Task ${index}`,
     description: `Description for task ${index}`,
-    dueDate: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    dueDate: dueDate,
     tags: tags,
     status: ["OPEN", "WORKING", "DONE", "OVERDUE"][
       Math.floor(Math.random() * 4)
