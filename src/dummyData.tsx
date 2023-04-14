ede612742a74f4c0800200071cf38fec9f553115
Ayman-Shakil192 <aymanshakilbms@gmail.com>
2023-04-15 02:57:59 +0530

    made use of API to add descriptions

--- src/dummyData.tsx
+++ src/dummyData.tsx
@@ -1,34 +1,48 @@
+import axios from "axios";
 import { ToDoItem } from "./components/ToDoList";
 
-export const dummyData: ToDoItem[] = new Array(80).fill(0).map((_, index) => {
-  const tags = [];
-  const randomTagCount = Math.floor(Math.random() * 4); // Assign any number of tags between 0 and 3
+const fetchData = async () => {
+  const response = await axios.get("https://dummyjson.com/todos?limit=150");
+  const todos = response.data.todos;
+
+  const tags: string[] = [];
+  const randomTagCount = Math.floor(Math.random() * 4);
   for (let j = 1; j <= randomTagCount; j++) {
     tags.push(`tag${j}`);
   }
-  const randomDays = Math.floor(Math.random() * 365);
-  const randomHours = Math.floor(Math.random() * 24);
-  const randomMinutes = Math.floor(Math.random() * 60);
-  const randomSeconds = Math.floor(Math.random() * 60);
-  const createdAt = new Date(
-    Date.now() -
-      randomDays * 24 * 60 * 60 * 1000 -
-      randomHours * 60 * 60 * 1000 -
-      randomMinutes * 60 * 1000 -
-      randomSeconds * 1000
-  ).getTime();
-  const dueDate = new Date(
-    createdAt + Math.floor(Math.random() * 5 + 1) * 24 * 60 * 60 * 1000
-  ).getTime();
-  return {
-    key: index,
-    created: createdAt,
-    title: `Task ${index}`,
-    description: `Description for task ${index}`,
-    dueDate: dueDate,
-    tags: tags,
-    status: ["OPEN", "WORKING", "DONE", "OVERDUE"][
-      Math.floor(Math.random() * 4)
-    ],
-  };
-});
+
+  return todos.map((todo: any, index: number) => {
+    const randomDays = Math.floor(Math.random() * 365);
+    const randomHours = Math.floor(Math.random() * 24);
+    const randomMinutes = Math.floor(Math.random() * 60);
+    const randomSeconds = Math.floor(Math.random() * 60);
+    const createdAt = new Date(
+      Date.now() -
+        randomDays * 24 * 60 * 60 * 1000 -
+        randomHours * 60 * 60 * 1000 -
+        randomMinutes * 60 * 1000 -
+        randomSeconds * 1000
+    ).getTime();
+    const dueDate = new Date(
+      createdAt + Math.floor(Math.random() * 5 + 1) * 24 * 60 * 60 * 1000
+    ).getTime();
+    const selectedTags: string[] = [];
+    const selectedTagCount = Math.floor(Math.random() * 4);
+    for (let j = 1; j <= selectedTagCount; j++) {
+      selectedTags.push(`tag${j}`);
+    }
+    return {
+      key: index,
+      created: createdAt,
+      title: `Task ${index}`,
+      description: todo.todo,
+      dueDate: dueDate,
+      tags: selectedTags,
+      status: ["OPEN", "WORKING", "DONE", "OVERDUE"][
+        Math.floor(Math.random() * 4)
+      ],
+    };
+  }) as ToDoItem[];
+};
+
+export const dummyData: ToDoItem[] = await fetchData();
